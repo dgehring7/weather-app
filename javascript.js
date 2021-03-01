8b0ee395c3e3474e5782c125daf979469bedd197
Dgehring <dgehring7@gmail.com>
2021-02-28 23:30:04 -0500

    finished pseudo code

---
+++ javascript.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+// Load local storage
+// Get users location
+// User clicks in search box and searches for city
+// Results appear next to search with City: name, date, an icon representation of weather conditions
+//      the temperature, the humidity, the wind speed, and the UV index for current weather
+// A color code let the user know if the conditions are favorable, moderate, or severe
+// Along with current weather, a five day forcast is displayed
+// Local storage collects search history, and when user clicks on previously searched it redisplays weather info
+});
