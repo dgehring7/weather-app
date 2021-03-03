b6814bcea5e2c7ae021321a9040146c716990855
Dgehring <dgehring7@gmail.com>
2021-03-03 18:09:56 -0500

    fixed API call

--- javascript.js
+++ javascript.js
@@ -3,17 +3,25 @@ $(document).ready(function() {
         let history=(!JSON.parse(localStorage.getItem("Search-history")))?
         JSON.parse(localStorage.getItem("Search-history")):[];
     });
+   
     let cities=[];
+    function getSearchCity () {
+        let city = $("#search-city").val();
+        city = city.trim();
+        getWeatherForcast(city);
+    }
    function getWeatherForcast(city) {
-       if (!city){
+       if (city === null) {
+           console.log("test")
            return;
        }
-       let owmEndpoint="http://api.openweathermap.org/data/2.5/weather?id={cityid}&APPID=60123747790be0a2e989708c4e9c45d6";
+       let owmEndpoint="http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=60123747790be0a2e989708c4e9c45d6";
        fetch(owmEndpoint)
        .then((response)=>response.json())
        .then((data)=>{
-           let futureForcastElem = document.querySelector("future-forcast");
-           futureForcastElem.innerHTML = <h4 class="mt-2">5-Day Future Forcast</h4>
+           console.log(data);
+           let futureForcastElem = document.querySelector("#future-forcast");
+           futureForcastElem.innerHTML = '<h4 class="mt-2">5-Day Future Forcast</h4>'
 
            futureForcastRowElm = document.createElement("div");
            futureForcastRowElm.className = '"row"';
@@ -24,7 +32,6 @@ $(document).ready(function() {
                     let columnElm = document.createElement('div');
                     columnElm.classList.add("col-md-2")
                     let cardElm = document.createElement('div');
-                    
                     cardElm.classList.add('card', 'bg-primary', 'text-white');
                 }
             }
@@ -32,10 +39,12 @@ $(document).ready(function() {
    }
 // Load local storage
 localStorage.getItem('name', 'weather')
+$("#search-btn").on("click", getSearchCity)
 // Get users location
 // User clicks in search box and searches for city
 // Results appear next to search with City: name, date, an icon representation of weather conditions
 //      the temperature, the humidity, the wind speed, and the UV index for current weather
+
 // fetch (api.openweathermap.org/data/2.5/weather?id={cityid}&APPID=60123747790be0a2e989708c4e9c45d6)
 //    api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=60123747790be0a2e989708c4e9c45d6
 // A color code let the user know if the conditions are favorable, moderate, or severe
